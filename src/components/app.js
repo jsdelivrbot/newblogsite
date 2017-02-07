1333287d41e17d5b73624ad26492d0d6349a8aeb
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-07 13:56:05 -0500

    added button to a new post page

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      React simple start
+      
+      {this.props.children}
       </div>
       
     );
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, IndexRoute } from 'react-router';
+
+import PostsIndex from './components/posts_index';
+import App from './components/app';
+import PostsNew from './components/posts_new';
+
+
+
+export default (
+		<Route path="/" component={App}>
+			<IndexRoute  component={PostsIndex} />
+			<Route path="posts/new" component={PostsNew} />
+		</Route>
+	);
